b8cdec5229d0c580c9d66d6050dc98c347d34f32
“Jozsef_Gulyas“ <jozsef.gulyas82@gmail.com>
2026-03-29 23:13:56 +0200

    order and polishing

--- Program.cs
+++ Program.cs
@@ -5,7 +5,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
+    .AddInteractiveServerComponents()
+    .AddCircuitOptions(options =>
+    {
+        options.DisconnectedCircuitMaxRetained = 5;
+        // ReconnectInterval is not available in this API; Blazor Server reconnection is handled by the client JS layer.
+        // To keep circuits open a bit longer, use DisconnectedCircuitRetentionPeriod if available:
+        // options.DisconnectedCircuitRetentionPeriod = TimeSpan.FromMinutes(1);
+    });
+
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
+builder.Services.AddServerSideBlazor(options => 
+{
+    options.DetailedErrors = true; // This allows the browser to see the actual C# exception
+});
 
 var gameStoreApiUrl = builder.Configuration["GameStoreApiUrl"] ?? 
     throw new Exception("GameStoreApiUrl is not set");
